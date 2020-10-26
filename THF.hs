176270b4ddf798fdf2f18c8c275ae2cfc07cbe73
JOSUERV99 <josuerojasvega@gmail.com>
2020-10-26 11:38:26 -0600

    <THF>: some bugs with lineLength and text spacing fixed!

--- THF.hs
+++ THF.hs
@@ -7,9 +7,13 @@ import Data.Map (Map, member, (!), fromList)
 enHyp :: HypMap
 enHyp = Data.Map.fromList [("controla",["con","tro","la"]), ("futuro",["fu","tu","ro"]),("presente",["pre","sen","te"]), ("futuro", ["fu", "tu", "ro"])]
 
+data SPFlag = SEPARAR | NOSEPARAR -- Separacion de palabras
+data ALFlag = AJUSTAR | NOAJUSTAR -- Ajuste de lineas
+
 type Line = [Token]
 data Token = Word String | Blank | HypWord String
              deriving (Eq,Show)
+
 get (Word w) = w
 get Blank = " "
 get (HypWord w) = w ++ "-"
@@ -18,21 +22,23 @@ string2line :: String -> Line
 string2line text = map (\wrd-> Word wrd) $ words text
 
 line2string :: Line -> String
-line2string line = unwords $ map (\tkn-> get tkn) line
+line2string line = unwords $ map (\tkn->if tkn/=Blank then get tkn else "") (reverse (dropWhile cleanFunc (reverse (dropWhile cleanFunc line))))
+ where cleanFunc = (\x->x==Blank)
 
 tokenLength :: Token -> Int
 tokenLength (Word w) = length w
 tokenLength (HypWord w) = length w + 1
-tokenLength (Blank) = 1
+tokenLength (Blank) = 1 
 
 lineLength :: Line -> Int
-lineLength line = sum (map (\tkn-> tokenLength tkn) line) + ( if (length line) <= 1 then 0 else (length line)-1 )
+lineLength line = sum (map (\tkn-> tokenLength tkn) line) + if (length line) <= 1 then 0 else (length line)-1
 
 breakLine :: Int -> Line -> (Line,Line)
 breakLine _ [] = ([], [])
 breakLine len line = (take breakIndex line, drop breakIndex line) 
  where
-  calcBreakIdx len (l:line) = if ((tokenLength l)<len && line/=[]) then 1 + (calcBreakIdx (len - (tokenLength l)) line) else 0
+  calcBreakIdx _ [] = 0
+  calcBreakIdx len (l:line) = if ((tokenLength l)<=len) then 1 + (calcBreakIdx (len - (tokenLength l)-1) line) else 0
   breakIndex = calcBreakIdx len line
 
 mergers :: [String] -> [(String, String)]
@@ -48,10 +54,10 @@ hyphenate wrdMap tkn = if ( member wrdWithoutPoints wrdMap ) then ( map (\(x, y)
   wrdWithoutPoints = takeWhile (\ch->ch /= '.') (get tkn)
 
 lineBreaks :: HypMap -> Int -> Line -> [(Line, Line)]
-lineBreaks wrdMap len line = if (lineLength line) < len then [breakLine len line] else map (\ele->(breakLine len ele)) resultList
+lineBreaks wrdMap len line = if (lineLength line) <= len then [breakLine len line] else map (\l->(breakLine len l)) lines
  where
   finalWrdSet = hyphenate wrdMap (last line)
-  resultList = line : (map (\(wrdStart, wrdEnd)->(init line)++([wrdStart,wrdEnd])) finalWrdSet)
+  lines = line : (map (\(wrdStart, wrdEnd)->(init line)++([wrdStart,wrdEnd])) finalWrdSet)
 
 insertBlanks :: Int -> [Token] -> [Token]
 insertBlanks _ [] = []
@@ -59,42 +65,25 @@ insertBlanks _ [wrd] = [wrd]
 insertBlanks spaces (w:wrds) = [w] ++ (take requiredSpaces (repeat Blank)) ++ insertBlanks (spaces - requiredSpaces) wrds
  where requiredSpaces = ceiling $ (fromIntegral spaces) / (fromIntegral (length wrds))
 
- {--
-	(j)	Finalmente, definir una función separarYalinear que reciba una tira
-	de caracteres y un tamaño de línea, y que devuelva una lista de tiras
-	de caracteres que no sean más largas que el tamaño especificado La 
-	función también recibe dos banderas. La primera indica si debe usar 
-	separación de palabras o no: el valor de NOSEPARAR indica que las 
-	líneas se crean sin separar palabras;  el valor de SEPARAR indica que
-	para cada línea se debe intentar separar la palabra que no cabe, 
-	usando la separación que incluya la mayor parte de la palabra en la 
-	línea anterior. La segunda bandera indica si se deben ajustar las 
-	líneas, esto es, se deben insertar espacios en blanco para que las 
-	líneas sean exactamente del tamaño dado; con excepción de la última 
-	línea, a la cual nunca se le inserta un espacio en blanco. Para esta 
-	segunda bandera, el valor de NOAJUSTAR indica que no deben insertarse
-	espacios en blanco para alinear el texto; si el valor es AJUSTAR 
-	entonces se insertan espacios en blanco en las líneas.
- --}
-
--- Separacion de palabras
-data SPFlag = SEPARAR | NOSEPARAR
--- Ajuste de lineas
-data ALFLAG = AJUSTAR | NOAJUSTAR
-
---------------------------------------------------------------------------------------
-separarYalinear maxLineLength NOSEPARAR NOAJUSTAR text = if (lineLength (snd currentLineSet)) > maxLineLength then [line2string (fst currentLineSet)] ++ nextLine else [line2string (snd currentLineSet)]
+separarYalinear :: Int -> SPFlag -> ALFlag -> String -> [String]
+---------------------------------------------------------------------------------------------
+separarYalinear maxLength NOSEPARAR NOAJUSTAR text = 
+ if (lineLength (snd currentLineSet)) > maxLength then [noAdjust (fst currentLineSet)] ++ nextLine else [noAdjust (fst currentLineSet)] ++ [noAdjust (snd currentLineSet)]
  where
-  currentLineSet = breakLine maxLineLength (string2line text)
-  nextLine = separarYalinear maxLineLength NOSEPARAR NOAJUSTAR (line2string (snd currentLineSet))
+  noAdjust line = line2string line
+  currentLineSet = breakLine maxLength (string2line text)
+  nextLine = separarYalinear maxLength NOSEPARAR NOAJUSTAR (line2string (snd currentLineSet))
+---------------------------------------------------------------------------------------------
+separarYalinear maxLength NOSEPARAR AJUSTAR text = 
+ if (lineLength (snd currentLineSet)) > maxLength then [adjustLine (fst currentLineSet)] ++ nextLine else [adjustLine (fst currentLineSet)] ++ [adjustLine (snd currentLineSet)]
+ where
+  adjustLine line = line2string $ insertBlanks (maxLength - lineLength line) line
+  currentLineSet = breakLine maxLength (string2line text)
+  nextLine = separarYalinear maxLength NOSEPARAR AJUSTAR (line2string (snd currentLineSet))
+---------------------------------------------------------------------------------------------
+separarYalinear maxLength SEPARAR NOAJUSTAR text = []
+---------------------------------------------------------------------------------------------
+separarYalinear maxLength SEPARAR AJUSTAR text = []
 
---------------------------------------------------------------------------------------
-separarYalinear maxLineLength SEPARAR   AJUSTAR   text = []
 
---------------------------------------------------------------------------------------
-separarYalinear maxLineLength SEPARAR NOAJUSTAR text = []
 
---------------------------------------------------------------------------------------
-separarYalinear maxLineLength NOSEPARAR AJUSTAR   text = []
-
---------------------------------------------------------------------------------------
