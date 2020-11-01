7dd5a37d2089318bc0cf99d9377f8dc7591d4186
JOSUERV99 <josuerojasvega@gmail.com>
2020-11-01 10:37:01 -0600

    <CLI and Files>: cli base to text aligner command handler and files to folder

--- CLI.hs
+++ CLI.hs
@@ -4,15 +4,56 @@
 	 by: JOSUERV99
 --}
 
+import Prelude hiding (null, lookup, map, filter)
+import Data.Map.Lazy hiding (sort,map,foldl)
+import Data.Map (fromList)
+import Data.Char
+import Data.List (sort,map)
 import System.IO
 
-type Dictionary = Map [Char] [[Char]]
+type State = Map String [String]
 
 main :: IO ()
-main = do ->
-       mainloop (fromList[])
+main = do 
+ mainloop (fromList[])
 
-mainloop :: Dictionary -> IO ()
-mainloop dict = do
+mainloop :: State -> IO ()
+mainloop state = do
   putStr ">> "
   inpStr <- getLine
+  let tokens  = words inpStr
+  let command = tokens!!0
+  let newState = fromList[]
+  
+  case command of
+   "load" -> do
+     --fileName <- tokens!!1
+     --inh <- openFile fileName ReadMode
+     --newState <- cargar inh state
+     --hClose inh
+     putStrLn $ "File " ++ " was loaded"
+     mainloop newState
+   "show" -> do
+     putStrLn "Showing..."
+     mainloop state     
+   "ins" -> do
+     putStrLn "Showing..."
+     mainloop newState
+   "save" -> do
+     putStrLn ">>> Filename output: "
+     --fileName <- getLine
+     --outh <- openFile fileName WriteMode
+     --descargar outh (sort (toList state))
+     --hClose outh
+     mainloop newState
+   "split" -> do
+     putStrLn "Splitting..."
+     mainloop newState
+   "splitf" -> do
+     putStrLn "Splitting to File..."
+     mainloop newState
+   "exit" -> do
+     putStrLn "Exitting..."
+   _ -> do
+     putStrLn $ "Unknown command ("++ command ++"): '" ++ inpStr ++ "'" 
+     mainloop state
